d5e550aad02e091d1c7be3e1b64df6442c6ff326
Hugo Hedlund <48281171+Hedlund01@users.noreply.github.com>
2024-01-19 15:45:43 +0100

    AA-23 Setup Nextjs and add an example page

---
+++ website/src/app/dashboard/layout.tsx
@@ -0,0 +1,40 @@
+import { CssVarsProvider, CssBaseline, Box } from "@mui/joy";
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+
+export default function Layout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <CssVarsProvider disableTransitionOnChange>
+            <CssBaseline />
+            <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
+                <Header />
+                <Sidebar />
+                <Box
+                    component="main"
+                    className="MainContent"
+                    sx={{
+                        px: { xs: 2, md: 6 },
+                        pt: {
+                            xs: 'calc(12px + var(--Header-height))',
+                            sm: 'calc(12px + var(--Header-height))',
+                            md: 3,
+                        },
+                        pb: { xs: 2, sm: 2, md: 3 },
+                        flex: 1,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        minWidth: 0,
+                        height: '100dvh',
+                        gap: 1,
+                    }}
+                >
+                    {children}
+                </Box>
+            </Box>
+        </CssVarsProvider>
+    )
+}
---
+++ website/src/app/page.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Sheet from '@mui/joy/Sheet';
+import Typography from '@mui/joy/Typography';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Input from '@mui/joy/Input';
+import Button from '@mui/joy/Button';
+import Link from '@mui/joy/Link';
+
+export default function Home() {
+  return (
+    <Sheet
+      sx={{
+        display: 'flex',
+        flexFlow: 'row nowrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+      }}
+    >
+   <p>hej</p>
+    </Sheet>
+  );
+}
