6b643477a40b6355580e2febaf7d2a7fc85adf37
Zecro <adrianfingal@gmail.com>
2024-02-06 14:47:37 +0100

    schema update

--- website/src/db/schema.sql
+++ website/src/db/schema.sql
@@ -7,10 +7,10 @@ CREATE TABLE verification_token (
 
 CREATE TABLE accounts (
     id SERIAL,
-    "userId" INTEGER NOT NULL,
+    userId INTEGER NOT NULL,
     type VARCHAR(255) NOT NULL,
     provider VARCHAR(255) NOT NULL,
-    "providerAccountId" VARCHAR(255) NOT NULL,
+    providerAccountId VARCHAR(255) NOT NULL,
     refresh_token TEXT,
     access_token TEXT,
     expires_at BIGINT,
@@ -23,9 +23,9 @@ CREATE TABLE accounts (
 
 CREATE TABLE sessions (
     id SERIAL,
-    "userId" INTEGER NOT NULL,
+    userId INTEGER NOT NULL,
     expires TIMESTAMPTZ NOT NULL,
-    "sessionToken" VARCHAR(255) NOT NULL,
+    sessionToken VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -33,7 +33,7 @@ CREATE TABLE users (
     id SERIAL,
     name VARCHAR(255),
     email VARCHAR(255),
-    "emailVerified" TIMESTAMPTZ,
+    emailVerified TIMESTAMPTZ,
     image TEXT,
     role VARCHAR(255),
     PRIMARY KEY (id)
@@ -48,22 +48,28 @@ CREATE TABLE Products(
     quantity INTEGER NOT NULL
 );
 
-CREATE TABLE Order_lines(
-    id SERIAL PRIMARY KEY,
-    "productId" INTEGER NOT NULL,
-    quantity INTEGER NOT NULL,
-    "orderId" SERIAL NOT NULL,
-    FOREIGN KEY ("productId") REFERENCES Products(id),
-    FOREIGN KEY ("orderId") REFERENCES Orders(id)
-);
-
 CREATE TABLE Orders(
     id SERIAL PRIMARY KEY,
-    "userId" INTEGER NOT NULL,
-    "createdAt" TIMESTAMPTZ DEFAULT NOW() NOT NULL,
-    "updatedAt" TIMESTAMPTZ DEFAULT NOW() NOT NULL,
-    "status" VARCHAR(255) NOT NULL,
-    "totalPrice" DECIMAL(10, 2) NOT NULL,
-    "totalQuantity" INTEGER NOT NULL,
-    FOREIGN KEY ("userId") REFERENCES users(id)
+    userId INTEGER NOT NULL,
+    createdAt TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    updatedAt TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    totalPrice DECIMAL(10, 2) NOT NULL,
+    totalQuantity INTEGER NOT NULL,
+    CONSTRAINT fk_user
+          FOREIGN KEY(userId)
+          REFERENCES users(id)
+);
+
+CREATE TABLE Order_lines(
+    id SERIAL PRIMARY KEY,
+    productId INTEGER NOT NULL,
+    quantity INTEGER NOT NULL,
+    orderId SERIAL NOT NULL,
+    CONSTRAINT fk_product
+          FOREIGN KEY(productId)
+          REFERENCES Products(id),
+    CONSTRAINT fk_order
+          FOREIGN KEY(orderId)
+          REFERENCES Orders(id)
 );
